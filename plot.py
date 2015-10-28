8a83cd184f36af4b2b2ffcc96d9ca8577e5c43e7
Pete Bachant <petebachant@gmail.com>
2015-10-28 13:47:52 -0400

    Add option to plot recovery

--- plot.py
+++ plot.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate plots.")
     parser.add_argument("plot", nargs="*", help="What to plot", default="perf",
                         choices=["perf", "wake", "blade-perf", "strut-perf",
-                                 "perf-curves", "perf-curves-exp"])
+                                 "perf-curves", "perf-curves-exp", "recovery"])
     parser.add_argument("--all", "-A", help="Generate all figures",
                         default=False, action="store_true")
     parser.add_argument("--save", "-s", help="Save to `figures` directory",
@@ -43,6 +43,8 @@ if __name__ == "__main__":
         plot_perf_curves(exp=False, save=args.save)
     if "perf-curves-exp" in args.plot or args.all:
         plot_perf_curves(exp=True, save=args.save)
+    if "recovery" in args.plot or args.all:
+        make_recovery_bar_chart(save=args.save)
 
     if not args.noshow:
         plt.show()
