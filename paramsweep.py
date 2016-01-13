97300e0ea58edc1d2893460b04a1a25c9d41a32e
Pete Bachant <petebachant@gmail.com>
2016-01-12 23:51:07 -0500

    Set default blockMesh nx to 36

--- paramsweep.py
+++ paramsweep.py
@@ -51,13 +51,13 @@ def log_perf(param="tsr", append=True):
     df.to_csv(fpath, index=False)
 
 
-def set_blockmesh_resolution(nx=32, ny=None, nz=None):
+def set_blockmesh_resolution(nx=36, ny=None, nz=None):
     """Set mesh resolution in `blockMeshDict`.
 
     If only `nx` is provided, the default resolutions for other dimensions are
     scaled proportionally.
     """
-    defaults = {"nx": 32, "ny": 32, "nz": 24}
+    defaults = {"nx": 36, "ny": 36, "nz": 27}
     if ny is None:
         ny = nx
     if nz is None:
