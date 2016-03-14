65453943d1b9c05c1627388295071cfc4449ce4c
Pete Bachant <petebachant@gmail.com>
2016-03-14 08:34:30 -0400

    Update what 'all' plots are for LES

--- plot.py
+++ plot.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-"""
-This script plots results from the UNH-RVAT turbinesFoam case.
-"""
+"""This script plots results from the UNH-RVAT turbinesFoam case."""
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -29,7 +27,7 @@ if __name__ == "__main__":
     parser.add_argument("--noshow", help="Do not call matplotlib show function",
                         default=False, action="store_true")
     parser.add_argument("-q", help="Quantities to plot", nargs="*",
-                        default=["alpha", "rel_vel_mag", "cc"])
+                        default=["alpha", "rel_vel_mag"])
     args = parser.parse_args()
 
     if args.save:
@@ -39,23 +37,23 @@ if __name__ == "__main__":
     if "wake" in args.plot or args.all:
         plot_meancontquiv(save=args.save)
         plot_kcont(save=args.save)
-    if "wake-profiles" in args.plot or args.all:
+    if "wake-profiles" in args.plot:
         plot_wake_profiles(save=args.save)
     if "wake-profiles-exp" in args.plot or args.all:
         plot_wake_profiles(exp=True, save=args.save)
     if "perf" in args.plot or args.all:
         plot_cp(save=args.save)
-    if "blade-perf" in args.plot or args.all:
+    if "blade-perf" in args.plot:
         plot_blade_perf(save=args.save, quantities=args.q)
-    if "strut-perf" in args.plot or args.all:
+    if "strut-perf" in args.plot:
         plot_strut_perf(save=args.save, quantities=args.q)
-    if "perf-curves" in args.plot or args.all:
+    if "perf-curves" in args.plot:
         plot_perf_curves(exp=False, save=args.save)
-    if "perf-curves-exp" in args.plot or args.all:
+    if "perf-curves-exp" in args.plot:
         plot_perf_curves(exp=True, save=args.save)
     if "recovery" in args.plot or args.all:
         make_recovery_bar_chart(save=args.save)
-    if "verification" in args.plot or args.all:
+    if "verification" in args.plot:
         plot_verification(save=args.save)
 
     if not args.noshow:
